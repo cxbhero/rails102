82fb9355807dac288f37f97cb328e09f9179e382
cxbhero <cxbhero@163.com>
2022-06-09 13:59:28 +0800

    add posts nested route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :groups
+  resources :groups do
+      resources :posts
+  end
   root 'groups#index'
 
 end
